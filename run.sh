d334d559f4d6389ea89ce490ca2282db25e19eec
ayusha <ayush.agarwal@sifive.com>
2020-01-21 13:53:24 +0530

    Files added for running riscv-qqemu

---
+++ run.sh
@@ -0,0 +1,5 @@
+export PATH="$PWD/qemu-riscv:$PATH"
+qemu-system-riscv64 -nographic -M virt -m 256M \
+-bios $PWD/opensbi-build/platform/qemu/virt/firmware/fw_jump.elf \
+-kernel $PWD/Image \
+-initrd $PWD/initramfs.cpio.gz
